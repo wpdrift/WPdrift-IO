bcb9dbb4f2174acf760e3b3e8ed681029c02ea32
Bhoot Biswas <bhoot.biswas@gmail.com>
2022-01-06 12:26:39 +0600

    Add new methods and fix minor issues

--- includes/class-runn-tools-subscription-reports.php
+++ includes/class-runn-tools-subscription-reports.php
@@ -4,20 +4,91 @@
  */
 class Runn_Tools_Subscription_Reports {
 	function __construct() {
-		add_action( 'wp_ajax_my_action', [ $this, 'my_action' ] );
+		// add_action( 'wp_ajax_my_action', [ $this, 'my_action' ] );
 		add_action( 'edd_subscription_post_create', [ $this, 'subscription_post_create' ], 10, 2 );
+		add_action( 'edd_subscription_cancelled', [ $this, 'decrease_total_mmr' ] );
+		add_action( 'edd_subscription_completed', [ $this, 'decrease_total_mmr' ] );
+		add_action( 'edd_subscription_expired', [ $this, 'decrease_total_mmr' ] );
+		add_action( 'edd_subscription_failing', [ $this, 'decrease_total_mmr' ] );
+		add_action( 'edd_recurring_set_subscription_status', [ $this, 'edd_recurring_set_subscription_status' ], 10, 3 );
 	}
 
 	public function my_action() {
+		$sub_id  = 5;
+		$edd_sub = new EDD_Subscription( $sub_id );
+
 		wp_send_json(
 			[
 				'$this->get_account_mrr( 1 )'  => $this->get_account_mrr( 1 ),
 				'$this->get_final_mrr()'       => $this->get_final_mrr(),
 				'$this->count_mrr_histories()' => $this->count_mrr_histories(),
+				'$edd_sub'                     => $edd_sub,
 			]
 		);
 	}
 
+	public function edd_recurring_set_subscription_status( $subscription_id, $status, $subscription ) {
+		if ( 'active' === $status ) {
+			$this->increase_total_mmr( $subscription_id );
+		} else {
+			$this->decrease_total_mmr( $subscription_id );
+		}
+	}
+
+	/**
+	 * [increase_total_mmr description]
+	 * @param  [type] $subscription_id               [description]
+	 * @return [type]                  [description]
+	 */
+	public function increase_total_mmr( $subscription_id ) {
+		$edd_sub = new EDD_Subscription( $subscription_id );
+
+		$history_arr = [
+			'customer_id' => $edd_sub->customer_id,
+			'created'     => current_time( 'mysql' ),
+		];
+
+		$final_mrr   = $this->get_final_mrr();
+		$account_mrr = $this->get_account_mrr( $edd_sub->customer_id );
+
+		$recurring_amount           = floatval( $edd_sub->recurring_amount );
+		$history_arr['account_mrr'] = $account_mrr + $recurring_amount;
+		$history_arr['delta']       = $recurring_amount;
+		$history_arr['total_mrr']   = $final_mrr + $recurring_amount;
+
+		$this->insert_mrr_history( $history_arr );
+	}
+
+	/**
+	 * [decrease_total_mmr description]
+	 * @param  [type] $subscription_id               [description]
+	 * @return [type]                  [description]
+	 */
+	public function decrease_total_mmr( $subscription_id ) {
+		$edd_sub = new EDD_Subscription( $subscription_id );
+
+		$history_arr = [
+			'customer_id' => $edd_sub->customer_id,
+			'created'     => current_time( 'mysql' ),
+		];
+
+		$final_mrr   = $this->get_final_mrr();
+		$account_mrr = $this->get_account_mrr( $edd_sub->customer_id );
+
+		$recurring_amount           = floatval( $edd_sub->recurring_amount );
+		$history_arr['account_mrr'] = $account_mrr - $recurring_amount;
+		$history_arr['delta']       = -$recurring_amount;
+		$history_arr['total_mrr']   = $final_mrr - $recurring_amount;
+
+		$this->insert_mrr_history( $history_arr );
+	}
+
+	/**
+	 * [subscription_post_create description]
+	 * @param  [type] $id                 [description]
+	 * @param  [type] $args               [description]
+	 * @return [type]       [description]
+	 */
 	public function subscription_post_create( $id, $args ) {
 		$history_arr = [
 			'customer_id' => $args['customer_id'],
@@ -35,9 +106,37 @@ class Runn_Tools_Subscription_Reports {
 		$this->insert_mrr_history( $history_arr );
 	}
 
+	/**
+	 * [subscription_cancelled description]
+	 * @param  [type] $subscription_id               [description]
+	 * @param  [type] $subscription                  [description]
+	 * @return [type]                  [description]
+	 */
+	public function subscription_cancelled( $subscription_id, $subscription ) {
+		$history_arr = [
+			'customer_id' => $subscription->customer_id,
+			'created'     => current_time( 'mysql' ),
+		];
+
+		$final_mrr   = $this->get_final_mrr();
+		$account_mrr = $this->get_account_mrr( $subscription->customer_id );
+
+		$recurring_amount           = floatval( $subscription->recurring_amount );
+		$history_arr['account_mrr'] = $account_mrr - $recurring_amount;
+		$history_arr['delta']       = -$recurring_amount;
+		$history_arr['total_mrr']   = $final_mrr - $recurring_amount;
+
+		$this->insert_mrr_history( $history_arr );
+	}
+
 	public function count_mrr_histories() {
 		global $wpdb;
 		return (int) $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->prefix}edd_mrr_history" );
+	}
+
+	public function subscription_completed( $id, $subscription ) {
+		// error_log( json_encode($args), 0 );
+		error_log( json_encode( $subscription ), 0 );
 	}
 
 	/**
